3bb214b83c74365d72df60a8a05a512e0eb6417f
RBarbush84 <93957735+RBarbush84@users.noreply.github.com>
2022-02-06 23:46:04 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+# Import libraries
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scraping
+
+# Set up flask app
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# HTML page/homepage route
+@app.route("/")
+def index():
+    # Find mars collection in db
+    mars = mongo.db.mars.find_one()
+# return html template
+    return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scraping.scrape_all()
+    mars.update_one({}, {"$set":mars_data}, upsert=True)
+    return redirect('/', code=302)
+
+if __name__ == "__main__":
+    app.run()
